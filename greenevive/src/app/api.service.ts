28aa461a738a2e59c1a88b39801925bc45aecc01
ComsaAndreea <52425269+ComsaAndreea@users.noreply.github.com>
2024-10-28 23:19:19 +0200

    Update api.service.ts

--- greenevive/src/app/api.service.ts
+++ greenevive/src/app/api.service.ts
@@ -12,19 +12,16 @@ export class ApiService {
   carURL = 'https://carbonfootprint1.p.rapidapi.com/CarbonFootprintFromCarTravel';
   flightURL = 'https://carbonfootprint1.p.rapidapi.com/CarbonFootprintFromFlight';
   publicTransportURL = 'https://carbonfootprint1.p.rapidapi.com/CarbonFootprintFromPublicTransit';
-  apiKey = '4a3f4ebcd4msh9832d9bdab7a457p1df500jsn56422ab19166';
-  //apiKey = '58ae19afa9msh62c0c6da7bd8222p1a8d7ejsn6133c617909f'; //asta merge
-  //apiKey = '4a3f4ebcd4msh9832d9bdab7a457p1df500jsn56422ab19166'; //Andreea final
-
+  apiKey = 'xxx'
   constructor(private http:HttpClient) { }
 
   getCarbonFootprintFromCar(distance: string,vehicle:string): Observable<any> {
     const headers = new HttpHeaders()
-    .set('X-RapidAPI-Key','4a3f4ebcd4msh9832d9bdab7a457p1df500jsn56422ab19166')
+    .set('X-RapidAPI-Key','xxx')
     .set('X-RapidAPI-Host','carbonfootprint1.p.rapidapi.com');
 
 
-    // 'X-RapidAPI-Key': '6c95d0fc40msh10ef7ecb5eb6bdap11f442jsnd57f13f60550',
+    // 'X-RapidAPI-Key': 'xxx'
     //   'X-RapidAPI-Host': 'carbonfootprint1.p.rapidapi.com'
 
 
@@ -36,7 +33,7 @@ export class ApiService {
   }
   getCarbonFootprintFromFlight(distance: string,type:string): Observable<any> {
     const headers = new HttpHeaders()
-    .set('X-RapidAPI-Key','4a3f4ebcd4msh9832d9bdab7a457p1df500jsn56422ab19166')
+    .set('X-RapidAPI-Key','xxx')
     .set('X-RapidAPI-Host','carbonfootprint1.p.rapidapi.com');
 
     const params = new HttpParams()
@@ -47,7 +44,7 @@ export class ApiService {
   }
   getCarbonFootprintFromPublicTransport(distance: string,type:string): Observable<any> {
     const headers = new HttpHeaders()
-    .set('X-RapidAPI-Key','4a3f4ebcd4msh9832d9bdab7a457p1df500jsn56422ab19166')
+    .set('X-RapidAPI-Key','xxx')
     .set('X-RapidAPI-Host','carbonfootprint1.p.rapidapi.com');
 
     const params = new HttpParams()
